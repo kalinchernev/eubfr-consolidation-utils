6cfeabc2ff3d53ecd6eb8c73e97440c3a9c5b200
Kalin Chernev <kalin.chernev@gmail.com>
2019-03-15 11:25:08 +0200

    Add push script

--- README.md
+++ README.md
@@ -3,6 +3,22 @@
 - Parses an input Excel file into a `.ndjson` one.
 - Reads `.ndjson` file and pours the data into Elasticsearch index
 
+## Get dependencies
+
+```sh
+$ yarn
+```
+
+Or:
+
+```sh
+$ npm install
+```
+
+## Environment settings
+
+Copy `.env.example` to `.env` and set the necessary values.
+
 ## Project files
 
 Input/output files are stored in `files` directory. For instance, `duplicates.xlsx` contains the input, whereas the `jobs.ndjson` will be the output.
@@ -20,3 +36,13 @@ $ INPUT_FILE=duplicates.xlsx node parseInputFile.js
 Where the `duplicates.xlsx` is placed in the `files` directory.
 
 The result will be stored in `/files/jobs.ndjson`.
+
+### Pushing data to Elasticsearch
+
+In order to work with the results in a more convenient way, one can load the resulting data from the previous step into an Elasticsearch domain.
+
+To do that, run the following:
+
+```sh
+$ node push.js
+```
--- push.js
+++ push.js
@@ -5,9 +5,16 @@ require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
+const { promisify } = require("util");
+
+const AWS = require("aws-sdk");
+const awscred = require("awscred");
+const connectionClass = require("http-aws-es");
+const elasticsearch = require("elasticsearch");
 const split2 = require("split2");
 const through2Batch = require("through2-batch");
-const elasticsearch = require("elasticsearch");
+
+const getUserCredentials = promisify(awscred.load);
 
 const SaveStream = require("./lib/SaveStream");
 
@@ -15,16 +22,40 @@ const SaveStream = require("./lib/SaveStream");
  * Push records/projects to a given Elasticsearch index.
  */
 const pushRecords = async () => {
-  const { ES_LOCAL_ENDPOINT, INDEX: index } = process.env;
+  const { ES_REMOTE_ENDPOINT, INDEX: index, TYPE: type } = process.env;
 
   try {
-    const options = { host: ES_LOCAL_ENDPOINT, index };
+    // Get user's AWS credentials and region
+    const credentials = await getUserCredentials();
+
+    const { region } = credentials;
+    const { accessKeyId, secretAccessKey } = credentials.credentials;
+
+    // Elasticsearch client configuration
+    const options = {
+      host: ES_REMOTE_ENDPOINT,
+      index,
+      connectionClass,
+      apiVersion: "6.3",
+      requestTimeout: 300000,
+      awsConfig: new AWS.Config({
+        accessKeyId,
+        secretAccessKey,
+        region
+      })
+    };
+
     const client = elasticsearch.Client(options);
 
-    const saveStream = new SaveStream({ objectMode: true, client, index });
+    const saveStream = new SaveStream({
+      objectMode: true,
+      client,
+      index,
+      type
+    });
 
     // We'll read results continuously in order to save memory.
-    const fileRead = fs.createReadStream(path.resolve("./results.ndjson"));
+    const fileRead = fs.createReadStream(path.resolve("./files/jobs.ndjson"));
 
     return new Promise((resolve, reject) => {
       fileRead
@@ -32,15 +63,13 @@ const pushRecords = async () => {
         .on("error", e => reject(e))
         .pipe(
           through2Batch.obj({ batchSize: 100 }, (batch, _, cb) => {
-            const improvedBatch = batch.map(item => item._source);
-
-            saveStream.write(improvedBatch, cb);
+            saveStream.write(batch, cb);
           })
         )
         .on("error", e => reject(e))
         .pipe(saveStream)
         .on("finish", () => {
-          console.log("Done");
+          console.log("Done.");
           resolve();
         });
     });
